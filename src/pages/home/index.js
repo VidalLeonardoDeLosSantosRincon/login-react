0a35f4a5816b6896e46e1272bd09bb8617ec4fb9
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2022-02-11 19:56:09 -0400

    importando login

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,13 @@
 import { Fragment } from "react";
 
+//components
+import {Login} from "../../components/login"
+
 export const Home = () =>{
     return(
         <Fragment>
             <div className="ctr_home">
-                Home
+                <Login/>
             </div>
         </Fragment>
     );
