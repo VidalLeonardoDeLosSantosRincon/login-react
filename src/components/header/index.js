8b2ec0e25f08f3904facf40a221cd8d0e8d792da
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2022-02-11 14:34:59 -0400

    creando header

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export const Header = () => {
+    return(
+        <Fragment>
+            <header className="_header">
+                header
+            </header>
+        </Fragment>
+    );
+};
