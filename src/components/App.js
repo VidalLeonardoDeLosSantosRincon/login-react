d69cf019ac78170bb723f2a1bcefd98e9f3ba215
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2022-02-11 14:42:38 -0400

    importando header y footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+//components
+import { Header } from "./header/";
+import { Footer } from "./footer/";
+
 function App({children}) {
   return (
     <div className="App">
+        <Header/>
         {children}
+        <Footer/>
     </div>
   );
 }
