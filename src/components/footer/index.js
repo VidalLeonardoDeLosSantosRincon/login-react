4ae2135b0c93add3b82a142fd249c116e2536889
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2022-02-11 14:41:11 -0400

    creando footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export const Footer = () => {
+    return(
+        <Fragment>
+            <footer className="_footer">
+                footer
+            </footer>
+        </Fragment>
+    );
+};
