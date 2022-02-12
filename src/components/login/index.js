a80326b28917af983cd871070db2b15e11d383cf
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2022-02-11 20:59:17 -0400

    conectando login con el store

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,8 +2,11 @@ import { Fragment } from "react";
 import { useState } from "react";
 import "./style.css";
 
-export const Login = () => {
-    const [ loginForm, setLoginForm ] = useState({email:"", password:""});
+//store redux 
+import {connect} from "react-redux";
+
+const LoginForm = ({login, dispatch}) => {
+    const [ loginForm, setLoginForm ] = useState(login);
     const {email, password} = loginForm;
 
     const onchangeForm = (e) =>{
@@ -12,10 +15,16 @@ export const Login = () => {
         setLoginForm({...loginForm, [_target.name]: _target.value});
     }
 
+    const logUser = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        dispatch({type:"LOG_USER", payload: loginForm});
+    }
+
     return (
         <Fragment>
             <div className="ctr_login">
-                <form id="form_login" action="#" autoComplete="off">                    
+                <form id="form_login" action="#" autoComplete="off" medthod="POST">                    
                     <h4>Email: {email}</h4>
                     <h4>Password: {password}</h4>
                     <input className="_fields" type="email" name="email" 
@@ -24,9 +33,23 @@ export const Login = () => {
                     <input className="_fields" type="password" name="password" 
                         placeholder="password" required
                         onChange={onchangeForm}/>
-                    <button id="btn_save">Save</button>
+                    <button id="btn_save" onClick={logUser}>Save</button>
                 </form>
             </div>
         </Fragment>
     );
 };
+
+const mapStateToprops = (state, ownPrps) =>{
+    //console.log('mapStateToProps', state);
+    //console.log('ownProps', ownPrps);
+    return {
+        login: state.loginReducer
+    }
+};
+
+const mapDispatchToProps = () => {
+
+};
+
+export const Login = connect(mapStateToprops)(LoginForm);
